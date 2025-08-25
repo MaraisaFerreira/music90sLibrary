48f429f7359f4dbd1233c8818b730d46faa69db1
Maraísa Ferreira <72461905+MaraisaFerreira@users.noreply.github.com>
2025-08-25 10:27:15 -0300

    create entity

---
+++ src/main/java/com/study/mf/model/Music.java
@@ -0,0 +1,79 @@
+package com.study.mf.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Music {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    private String artist;
+    @Column(nullable = false)
+    private Integer year;
+
+    public Music(){}
+
+    public Music(Long id, String name, String artist, Integer year){
+        this.id = id;
+        this.name = name;
+        this.artist = artist;
+        this.year = year;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+
+    public void setArtist(String artist){
+        this.artist = artist;
+    }
+
+    public Integer getYear(){
+        return year;
+    }
+
+    public void setYear(Integer year){
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Music music)) return false;
+        return Objects.equals(getId(), music.getId()) && Objects.equals(getName(), music.getName()) && Objects.equals(getArtist(), music.getArtist()) && Objects.equals(getYear(), music.getYear());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getArtist(), getYear());
+    }
+
+    @Override
+    public String toString() {
+        return "Music { " +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", artist='" + artist + '\'' +
+                ", year=" + year +
+                " }";
+    }
+}
