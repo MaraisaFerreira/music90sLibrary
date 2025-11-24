acef0bc4e9d7d14d2a6dd0a882288c50d2bb72e7
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2025-11-24 11:40:45 -0300

    change cls name

--- src/main/java/com/study/mf/Music90sLibraryApplication.java
+++ src/main/java/com/study/mf/Music90sLibraryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Startup {
+public class Music90sLibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Startup.class, args);
+		SpringApplication.run(Music90sLibraryApplication.class, args);
 	}
 
 }
