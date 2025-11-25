3495a0c8bffeaed46465f3870951f579e9561f6d
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2025-11-25 11:17:53 -0300

    feat. add cors origin

--- src/main/java/com/study/mf/config/WebConfig.java
+++ src/main/java/com/study/mf/config/WebConfig.java
@@ -1,12 +1,17 @@
 package com.study.mf.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${cors.origins}")
+    private String origins;
+
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.ignoreAcceptHeader(false)
@@ -15,5 +20,15 @@ public class WebConfig implements WebMvcConfigurer {
             .mediaType("json", MediaType.APPLICATION_JSON)
             .mediaType("xml", MediaType.APPLICATION_XML)
             .mediaType("yaml", MediaType.APPLICATION_YAML);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String[] allowedOrigins = origins.split(",");
+
+        registry.addMapping("/**")
+            .allowCredentials(true)
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedOrigins(allowedOrigins);
     }
 }
